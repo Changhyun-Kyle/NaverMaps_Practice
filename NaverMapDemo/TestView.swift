23090473cf1fc478b101459b9ba7ba756f5892da
Changhyun Kang <changhyun.kyle@gmail.com>
2023-01-28 01:59:21 +0900

    [Feat] 검색 바 구현

---
+++ NaverMapDemo/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  NaverMapDemo
+//
+//  Created by 강창현 on 2023/01/27.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
